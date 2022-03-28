137d9c929b53c5bf52ce4fe3973634eb01dcd911
balisong7000 <2214555187@qq.com>
2022-03-28 14:55:19 +0800

    123

--- src/main/java/community/community/dao/GitHubUser.java
+++ src/main/java/community/community/dao/GitHubUser.java
@@ -1,40 +1,10 @@
 package community.community.dao;
 
+import lombok.Data;
+
+@Data
 public class GitHubUser {
     private String  name;
     private Integer id;
     private  String bao;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getBao() {
-        return bao;
-    }
-
-    public void setBao(String bao) {
-        this.bao = bao;
-    }
-
-    @Override
-    public String toString() {
-        return "GitHubuser{" +
-                "name='" + name + '\'' +
-                ", id=" + id +
-                ", bao='" + bao + '\'' +
-                '}';
-    }
 }
